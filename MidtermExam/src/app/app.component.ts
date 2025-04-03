ff95f2a98f26d3ae21c476e29d830875825a12f7
Cedric <20229681@s.ubaguio.edu>
2025-04-03 18:07:43 +0800

    Initial commit

---
+++ MidtermExam/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MidtermExam';
+}
---
+++ MidtermExam/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AboutComponent } from './about/about.component';
+import { AddResourcesComponent } from './add-resources/add-resources.component';
+import { ProgrammingComponent } from './categories/programming/programming.component';
+import { DesignComponent } from './categories/design/design.component';
+import { MathComponent } from './categories/math/math.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    CategoriesComponent,
+    AboutComponent,
+    AddResourcesComponent,
+    ProgrammingComponent,
+    DesignComponent,
+    MathComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule // Include the routing module here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule { }
