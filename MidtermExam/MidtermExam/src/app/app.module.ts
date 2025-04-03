3b15aedfd76835c670859113f872e2afde109a27
x <x@f112.ub.lan>
2025-04-03 15:41:48 +0800

    commiting set up in the routes

--- MidtermExam/MidtermExam/src/app/app.module.ts
+++ MidtermExam/MidtermExam/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component'; // Import the FooterComponent
+import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app.routing.module'; // Import the routing module
+import { HomeComponent } from './home/home.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { AboutComponent } from './about/about.component';
 import { AddResourcesComponent } from './add-resources/add-resources.component';
+import { ProgrammingComponent } from './categories/programming/programming.component';
+import { DesignComponent } from './categories/design/design.component';
+import { MathComponent } from './categories/math/math.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +19,13 @@ import { AddResourcesComponent } from './add-resources/add-resources.component';
     HeaderComponent,
     NavigationComponent,
     FooterComponent,
+    HomeComponent,
     CategoriesComponent,
     AboutComponent,
-    AddResourcesComponent
+    AddResourcesComponent,
+    ProgrammingComponent,
+    DesignComponent,
+    MathComponent
   ],
   imports: [
     BrowserModule,
--- MidtermExam/MidtermExam/src/app/app.routing.module.ts
+++ MidtermExam/MidtermExam/src/app/app.routing.module.ts
@@ -4,12 +4,23 @@ import { HomeComponent } from './home/home.component'; // Import your HomeCompon
 import { CategoriesComponent } from './categories/categories.component';
 import { AboutComponent } from './about/about.component';
 import { AddResourcesComponent } from './add-resources/add-resources.component';
+import { ProgrammingComponent } from './categories/programming/programming.component'; // Import ProgrammingComponent
+import { DesignComponent } from './categories/design/design.component'; // Import DesignComponent
+import { MathComponent } from './categories/math/math.component'; // Import MathComponent
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // Home route
-  { path: 'categories', component: CategoriesComponent }, // Categories route
-  { path: 'about', component: AboutComponent }, // About route
-  { path: 'add-resources', component: AddResourcesComponent }, // Add Resources route
+  { path: '', component: HomeComponent }, // Main route for Home
+  { path: 'about', component: AboutComponent }, // Main route for About
+  { path: 'add-resources', component: AddResourcesComponent }, // Main route for Add Resources
+  { 
+    path: 'categories', 
+    component: CategoriesComponent, // Parent route for Categories
+    children: [
+      { path: 'programming', component: ProgrammingComponent }, // Child route for Programming
+      { path: 'design', component: DesignComponent }, // Child route for Design
+      { path: 'math', component: MathComponent } // Child route for Math
+    ]
+  }
 ];
 
 @NgModule({
