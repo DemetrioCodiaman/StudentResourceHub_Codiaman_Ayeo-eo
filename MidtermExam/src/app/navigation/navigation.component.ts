1c17c39eef7c1d967b212929427e626e45f94867
Cedric <20229681@s.ubaguio.edu>
2025-04-03 21:35:53 +0800

    implement responsive design improvements

--- MidtermExam/src/app/navigation/navigation.component.ts
+++ MidtermExam/src/app/navigation/navigation.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { AddResourcesComponent } from '../add-resources/add-resources.component';
 import { AboutComponent } from '../about/about.component';
 import { CategoriesComponent } from '../categories/categories.component';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink, RouterLinkActive,],
+  imports: [RouterModule, RouterLink, RouterLinkActive,],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
